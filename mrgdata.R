a50f7c6ae3cafad77e91f0c16cdc54ce20e88aaa
Zhang Liang <psychelzh@gmail.com>
2017-12-19 13:51:46 +0800

    ENHANCE: save user information and ability score separately

--- mrgdata.R
+++ mrgdata.R
@@ -7,6 +7,8 @@ library(stringr)
 
 # configurations
 data_dir <- "里水镇"
+out_user_vars <- c("userId", "姓名", "性别", "学校", "出生日期", "年级", "班", "语文", "数学")
+out_data_vars <- c("userId", "createDate", "abname", "abscore")
 user_files <- list.files(data_dir, "xls$", full.names = TRUE)
 exam_file <- list.files(data_dir, "xlsx$", full.names = TRUE)
 ability_file <- list.files(data_dir, "csv$", full.names = TRUE)
@@ -15,7 +17,10 @@ ability_file <- list.files(data_dir, "csv$", full.names = TRUE)
 user_info <- user_files %>%
   map(read_excel) %>%
   reduce(rbind) %>%
-  filter(!is.na(userId)) %>%
+  filter(
+    !is.na(userId),
+    !is.na(姓名)
+  ) %>%
   mutate(
     userId = parse_integer(userId),
     外部编码 = parse_integer(外部编码)
@@ -41,21 +46,17 @@ exam_score <- excel_sheets(exam_file) %>%
     数学 = parse_double(数学)
   )
 
+# merge exam score and user information
+user_info_full <- user_info %>%
+  left_join(exam_score) %>%
+  select(one_of(out_user_vars))
+write_rds(user_info_full, file.path(data_dir, "user_info.rds"))
+
 # load ability score
 ability_score <- read_csv(ability_file) %>%
   rename(
-    姓名 = userName,
-    性别 = userGender,
-    外部编码 = outCode,
-    学号 = userNo
-  )
-
-# join results
-user_data <- user_info %>%
-  left_join(exam_score) %>%
-  left_join(ability_score) %>%
-  select(-abId) %>%
-  filter(!is.na(name)) %>%
-  unique() %>%
-  spread(name, ability_score)
-write_rds(user_data, file.path(data_dir, "user_data.rds"))
+    abname = name,
+    abscore = ability_score
+  ) %>%
+  select(one_of(out_data_vars))
+write_rds(ability_score, file.path(data_dir, "user_ability.rds"))
