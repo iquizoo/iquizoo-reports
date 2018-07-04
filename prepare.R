ae7567514da369af3297f2bd4943ad0ea99cf4c7
Liang Zhang <psychelzh@outlook.com>
2018-07-04 14:07:50 +0800

    Refactor part of the workflow
    Now we store data as many relational tables as different worksheets of a workbook.

--- prepare.R
+++ prepare.R
@@ -25,18 +25,21 @@ library(glue)
 #' @param loc Location of data, i.e., the specific data folder
 #' @return Returns 0 if succeeded.
 main <- function(loc) {
+  # environmental settings ----
   data_dir <- file.path("datasets", loc)
   info_dir <- file.path("_info")
   # load configurations
   configs <- read_yaml(file.path(data_dir, "config.yml"), fileEncoding = "UTF-8")
   # set the directory where the result data go
   res_dir <- configs$goal$res_path
-  # use norm to correct data or not, if not, will use standardized score only
-  norm_correct <- configs$score_correction$norm
-  if (norm_correct) {
-    mod_tests_sp <- configs$score_correction$special
-    mod_tests_com <- configs$score_correction$common
-  }
+  if (!dir.exists(res_dir)) dir.create(res_dir)
+  # score correction method
+  correct_method <- configs$score_correction$method
+  # norm_correct <- configs$score_correction$norm
+  # if (norm_correct) {
+  #   mod_tests_sp <- configs$score_correction$special
+  #   mod_tests_com <- configs$score_correction$common
+  # }
   # TABLE: ability name informations
   abilities <- tribble(
     ~abId, ~abParent, ~abName,        ~abNameEn,
@@ -72,18 +75,7 @@ main <- function(loc) {
   breaks <- qnorm(c(0, 0.3, 0.7, 0.9, 1)) * 15 + 100
   labels <- LETTERS[4:1]
 
-  # clean data / correct data ----
-  # load dataset
-  data_origin <- read_excel(
-    file.path(data_dir, configs$data_file), guess_max = 1048576 # maximal number of rows
-  )
-  # separate original data into three relational tables
-  # TABLE: exercises'/tasks' information
-  exercises <- data_origin %>%
-    select(one_of(key_vars[["exercise"]])) %>%
-    unique()
-
-  # load ability map tables
+  # load ability map tables ----
   abilities_info <- read_excel(file.path(info_dir, "abilities.xlsx"))
   exercises_info <- read_excel(file.path(info_dir, "exerciseInfo.xlsx"))
   # TABLE: ability map between exercises and abilities
@@ -102,67 +94,101 @@ main <- function(loc) {
     filter(!is.na(abId)) %>%
     select(-abNameOld, -type) %>%
     add_column(mapId = 1:nrow(.), .before = 1)
+
+  # clean data / correct data ----
+  # load dataset
+  data_origin <- read_excel(
+    file.path(data_dir, configs$data_file), guess_max = 1048576 # maximal number of rows
+  )
   # data correction
-  if (norm_correct) {
-    # read exercise code information
-    task_codes <- read_html(file.path(info_dir, "exercise.html")) %>%
-      html_node("table") %>%
-      html_table(header = TRUE) %>%
-      rename(excerciseId = excerciseid)
-    # common norms
-    data_norms_common <- read_excel(
-      file.path(info_dir, "norm_convert_release.xlsx"), skip = 1
-    ) %>%
-      mutate(code = parse_integer(交互题CODE)) %>%
-      select(-交互题CODE) %>%
-      rename(
-        title.com = 描述说明,
-        avg.com = 平均数,
-        std.com = 标准差
+  scores_corrected <- switch(
+    correct_method,
+    none = data_origin %>%
+      mutate(stdScore = standardScore),
+    iquizoo = {
+      # read exercise code information
+      task_codes <- read_html(file.path(info_dir, "exercise.html")) %>%
+        html_node("table") %>%
+        html_table(header = TRUE) %>%
+        rename(excerciseId = excerciseid)
+      # common norms
+      norms_common_global <- read_excel(
+        file.path(info_dir, "norm_common_global.xlsx"), skip = 1
       ) %>%
-      filter(!is.na(code))
-    # special norms
-    data_norms_special <- read_excel(
-      file.path(info_dir, "norm_convert_release_special.xlsx"), skip = 1
-    ) %>%
-      mutate(excerciseId = parse_double(交互题CODE)) %>%
-      select(-交互题CODE) %>%
-      rename(
-        title.sp = 描述说明,
-        avg.sp = 平均数,
-        std.sp = 标准差
-      ) %>%
-      filter(!is.na(excerciseId))
-    # correct data by norms
-    scores_corrected <- data_origin %>%
-      left_join(task_codes, by = "excerciseId") %>%
-      left_join(data_norms_common, by = "code") %>%
-      left_join(data_norms_special, by = "excerciseId") %>%
-      mutate(
-        stdScore = case_when(
-          excerciseId %in% mod_tests_sp ~
-            (asin(sqrt(index)) - avg.sp) / std.sp * 15 + 100,
-          # note: use 'magic number' here for simplicity
-          excerciseId == mod_tests_com ~
-            (asin(sqrt(index)) - avg.com) / std.com * 15 - 14.4 + 100,
-          TRUE ~ standardScore
-        )
+        mutate(code = parse_integer(交互题CODE)) %>%
+        select(-交互题CODE) %>%
+        rename(
+          title.com = 描述说明,
+          avg.com = 平均数,
+          std.com = 标准差
+        ) %>%
+        filter(!is.na(code))
+      # construct header
+      agewise_header <- read_excel(
+        file.path(info_dir, "norm_agewise.xlsx"), skip = 2,
+        col_names = FALSE, n_max = 2
       )
-  } else {
-    scores_corrected <- data_origin %>%
+      # fill missing header
+      miss_header <- is.na(agewise_header[1, ])
+      agewise_header[1, miss_header] <- agewise_header[1, which(miss_header) - 1]
+      agewise_header_chr <- paste(agewise_header[1, ], agewise_header[2, ], sep = "_") %>%
+        str_replace("_NA", "")
+      norms_common_agewise <- read_excel(
+        file.path(info_dir, "norm_agewise.xlsx"), skip = 4,
+        col_names = agewise_header_chr
+      )
+      # special norms
+      norms_special_global <- read_excel(
+        file.path(info_dir, "norm_special_global.xlsx"), skip = 1
+      ) %>%
+        mutate(excerciseId = parse_double(交互题CODE)) %>%
+        select(-交互题CODE) %>%
+        rename(
+          title.sp = 描述说明,
+          avg.sp = 平均数,
+          std.sp = 标准差
+        ) %>%
+        filter(!is.na(excerciseId))
+      # correct data by norms
+      data_origin %>%
+        left_join(task_codes, by = "excerciseId") %>%
+        left_join(norms_common_global, by = "code") %>%
+        left_join(norms_special_global, by = "excerciseId") %>%
+        mutate(
+          stdScore = case_when(
+            excerciseId %in% configs$score_correction$args$special ~
+              (asin(sqrt(index)) - avg.sp) / std.sp * 15 + 100,
+            # note: use 'magic number' here for simplicity
+            excerciseId %in% configs$score_correction$args$common ~
+              (asin(sqrt(index)) - avg.com) / std.com * 15 - 14.4 + 100,
+            TRUE ~ standardScore
+          )
+        )
+    },
+    scale = data_origin %>%
       group_by(excerciseId, grade) %>%
       mutate(stdScore = scale(index) * 15 + 100)
+  )
+  # correction for user information
+  if (!is.null(configs$user_correction)) {
+    user_correction_varnames <- names(configs$user_correction$replace)
+    for (user_correction_varname in user_correction_varnames) {
+      scores_corrected[[user_correction_varname]] <-
+        configs$user_correction$replace[[user_correction_varname]]
+    }
   }
-  # read extra datasets
-  if (!is.null(configs$data_extra)) {
-    # TABLE: extra database
-    assign(
-      configs$data_extra$name,
-      read_excel(file.path(data_dir, configs$data_extra$file))
+  # this is a special correction, currently only used for 'erxiao' dataset
+  if (!is.null(configs$user_correction$special)) {
+    scores_corrected <- switch(
+      configs$user_correction$special,
+      jcAccount = scores_corrected %>%
+        mutate(
+          cls = parse_double(str_sub(jcAccount, 6, 7))
+        )
     )
   }
   # data cleanse: remove duplicates and outliers based on boxplot rule
-  user_scores_clean <- scores_corrected %>%
+  scores_clean <- scores_corrected %>%
     # remove duplicates
     group_by(userId, excerciseId) %>%
     mutate(occurrence = row_number(desc(stdScore))) %>%
@@ -178,8 +204,14 @@ main <- function(loc) {
     ) %>%
     ungroup() %>%
     unique()
+
+  # separate original data into three relational tables ----
+  # TABLE: exercises'/tasks' information
+  exercises <- scores_clean %>%
+    select(one_of(key_vars[["exercise"]])) %>%
+    unique()
   # TABLE: users' information
-  users <- user_scores_clean %>%
+  users <- scores_clean %>%
     select(one_of(key_vars[["user"]])) %>%
     unique() %>%
     mutate(
@@ -187,7 +219,7 @@ main <- function(loc) {
       cls = glue("{cls}班")
     )
   # TABLE: scores of all users on all tasks/exercises
-  scores <- user_scores_clean %>%
+  scores <- scores_clean %>%
     add_column(scoreId = 1:nrow(.), .before = 1) %>%
     select(one_of(key_vars[["score"]]))
 
@@ -214,15 +246,23 @@ main <- function(loc) {
   ability_scores <- rbind(components_scores, total_scores) %>%
     add_column(abscoreId = 1:nrow(.), .before = 1)
 
-  # side effects: output all ability scores after clensing
-  write_xlsx(
-    list(
-      ability_scores = ability_scores,
-      users = users,
-      abilities = abilities
-    ),
-    file.path(res_dir, glue("{loc}.xlsx"))
-  )
+  # read extra datasets ----
+  if (!is.null(configs$data_extra)) {
+    # TABLE: extra database
+    assign(
+      configs$data_extra$name,
+      read_excel(file.path(data_dir, configs$data_extra$file))
+    )
+  }
+
+  # side effects: save all useful information as Excel files ----
+  # future plan will be to store them to the local/remote MySQL database
+  output_tables <- configs$goal$tables
+  outputs <- list()
+  for (output_table in output_tables) {
+    outputs[[output_table]] <- eval(as.symbol(output_table))
+  }
+  write_xlsx(outputs, file.path(res_dir, glue("{loc}.xlsx")))
 }
 
 if (interactive()) {
