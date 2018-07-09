7792f1b97de0bf3094449a3563ddd546dcca6926
Liang Zhang <psychelzh@outlook.com>
2018-07-09 23:18:19 +0800

    Move PRIMARY/FOREIGN KEY contraints below the column specification
    It will throw an error when these keys are specified before column specification in 'SQLite'.

--- sql/abilities.sql
+++ sql/abilities.sql
@@ -1,9 +1,9 @@
 CREATE TABLE abilities (
-    PRIMARY KEY (abId),
     abId    INT         NOT NULL,
     parent  INT         NOT NULL, -- parent ability, use 0 if there is no parent
     name    VARCHAR(10), -- ability name
     name_en VARCHAR(30), -- ability english name
-            CONSTRAINT chk_abId
-            CHECK (abId > 0)
+    PRIMARY KEY (abId),
+        CONSTRAINT chk_abId
+        CHECK (abId > 0)
 );
--- sql/exercises.sql
+++ sql/exercises.sql
@@ -2,7 +2,7 @@ CREATE TABLE exercises (
     exerciseId INT NOT NULL,
     name VARCHAR(100), -- exercise name
     title VARCHAR(100), -- exercise title displayed
+    PRIMARY KEY (exerciseId),
         CONSTRAINT chk_id
-        CHECK (exerciseId > 0),
-        PRIMARY KEY (exerciseId)
+        CHECK (exerciseId > 0)
 );
