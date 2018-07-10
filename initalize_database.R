2b07baf8d4c18399bda07c09de3d541d18219167
Liang Zhang <psychelzh@outlook.com>
2018-07-10 09:51:26 +0800

    Move database preparation part to an independent script

---
+++ initalize_database.R
@@ -0,0 +1,53 @@
+# Copyright (C) 2018 Liang Zhang - All Rights Reserved
+
+# @author Liang Zhang <psychelzh@outlook.com>
+
+# load packages and settings ----
+library(tidyverse)
+library(dbplyr)
+library(readxl)
+library(DBI)
+library(RSQLite)
+
+# database preparations ----
+# open connection
+iquizoo_db <- dbConnect(SQLite(), dbname = "iquizoo.sqlite")
+# initializing database tables
+creation_query_folder <- "sql"
+creation_query_files <- list.files(creation_query_folder, "\\.sql")
+for (creation_query_file in creation_query_files) {
+  table_name <- tools::file_path_sans_ext(creation_query_file)
+  if (!db_has_table(iquizoo_db, table_name)) {
+    dbSendQuery(iquizoo_db, "BEGIN;")
+    dbSendQuery(
+      iquizoo_db, read_file(
+        file.path(creation_query_folder, creation_query_file)
+      )
+    )
+    dbSendQuery(iquizoo_db, "COMMIT;")
+  }
+}
+# prepare exercises, abilities
+info_dir <- file.path("_info")
+# update abilities
+abilities <- read_excel(file.path(info_dir, "abilities.xlsx"))
+db_insert_into(iquizoo_db, "abilities", abilities, overwrite = TRUE)
+# update exercises and exercises map
+exercises_joined <- read_excel(file.path(info_dir, "exerciseInfo.xlsx")) %>%
+  mutate(excerciseId = parse_double(ID)) %>%
+  gather(type, abName, ability_blai, ability_math) %>%
+  filter(abName != "null") %>%
+  left_join(abilities, by = c("abName" = "name")) %>%
+  filter(!is.na(abId)) %>%
+  select(excerciseId, abId, name, title) %>%
+  rename(exerciseId = excerciseId) %>%
+  unique()
+exercises <- exercises_joined %>%
+  select(exerciseId, name, title) %>%
+  unique()
+exercise_ability <- exercises_joined %>%
+  select(exerciseId, abId) %>%
+  unique()
+db_insert_into(iquizoo_db, "exercises", exercises, overwrite = TRUE)
+db_insert_into(iquizoo_db, "exercise_ability", exercise_ability, overwrite = TRUE)
+dbDisconnect(iquizoo_db)
