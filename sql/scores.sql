47c8caa19cb117711cf04c6078feb2bcd0703a80
Liang Zhang <psychelzh@outlook.com>
2018-07-11 13:44:34 +0800

    Enhance scores table by adding proper constraint to createTime column

--- sql/scores.sql
+++ sql/scores.sql
@@ -1,10 +1,12 @@
 CREATE TABLE scores (
-    userId     INT         NOT NULL,
-    exerciseId INT         NOT NULL,
-    createTime VARCHAR(30) NOT NULL,
-    score      NUMERIC     NOT NULL,
+    userId     INT      NOT NULL,
+    exerciseId INT      NOT NULL,
+    createTime CHAR(19) NOT NULL,
+    score      NUMERIC  NOT NULL,
     PRIMARY KEY (userId, exerciseId, createTime),
     FOREIGN KEY (exerciseId) REFERENCES exercises (exerciseId),
         CONSTRAINT score_id_positive
-        CHECK (userId > 0 AND exerciseId > 0)
+        CHECK (userId > 0 AND exerciseId > 0),
+        CONSTRAINT score_create_time_chk
+        CHECK (createTime LIKE '____-__-__ __:__:__') -- 'YYYY-mm-dd HH:MM:SS'
 )
