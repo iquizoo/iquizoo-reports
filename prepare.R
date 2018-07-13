75eefc7e0db2484956e26c9c540e8df678b55524
Liang Zhang <psychelzh@outlook.com>
2018-07-13 10:36:20 +0800

    Refactor codes to take advantage of `R` `config` package to load configs

--- prepare.R
+++ prepare.R
@@ -18,23 +18,17 @@ library(glue)
 library(DBI)
 library(RSQLite)
 
-#' Main function used to prepare datasets
-#'
-#' Now the main function is a generic function to do preparation works for
-#' datasets from any region. The most important work here is to do score
-#' correction for some tasks. Configurations are load from a "config.yml" file
-#' from the dataset directory.
-#'
-#' @param loc Location of data, i.e., the specific data folder
-main <- function(loc) {
-  # open connection
-  iquizoo_db <- dbConnect(SQLite(), dbname = "iquizoo.sqlite")
-  # ensure database is disconnected after processing
-  on.exit(dbDisconnect(iquizoo_db))
+base_configs <- config::get()
+base_datasets <- base_configs$datasets
+
+# open connection
+iquizoo_db <- dbConnect(SQLite(), dbname = "iquizoo.sqlite")
+
+for (base_dataset in base_datasets) {
   # environmental settings ----
-  data_dir <- file.path("datasets", loc)
   # load configurations
-  configs <- read_yaml(file.path(data_dir, "config.yml"), fileEncoding = "UTF-8")
+  dataset_configs <- config::merge(base_configs, config::get(config = base_dataset))
+  data_dir <- file.path(dataset_configs$data_path, base_dataset)
   # future work will be to create table with SQL queries
   key_vars <- list(
     user = c("userId", "name", "sex", "school", "grade", "class"),
@@ -47,14 +41,14 @@ main <- function(loc) {
 
   # load dataset ----
   data_origin <- switch(
-    tools::file_ext(configs$data_file),
+    tools::file_ext(dataset_configs$data_file),
     xls = ,
     xlsx = read_excel(
-      file.path(data_dir, configs$data_file),
+      file.path(data_dir, dataset_configs$data_file),
       guess_max = 1048576 # maximal number of rows
     ),
     json = jsonlite::read_json(
-      file.path(data_dir, configs$data_file), simplifyVector = TRUE
+      file.path(data_dir, dataset_configs$data_file), simplifyVector = TRUE
     ) %>%
       as.tbl() %>%
       mutate(createTime = lubridate::as_datetime(createTime))
@@ -64,17 +58,17 @@ main <- function(loc) {
     rename(exerciseId = excerciseId, class = cls)
 
   # correction for user information ----
-  if (!is.null(configs$user_correction)) {
-    user_correction_varnames <- names(configs$user_correction$replace)
+  if (!is.null(dataset_configs$user_correction)) {
+    user_correction_varnames <- names(dataset_configs$user_correction$replace)
     for (user_correction_varname in user_correction_varnames) {
       data_origin[[user_correction_varname]] <-
-        configs$user_correction$replace[[user_correction_varname]]
+        dataset_configs$user_correction$replace[[user_correction_varname]]
     }
   }
   # this is a special correction, currently only used for 'erxiao' dataset
-  if (!is.null(configs$user_correction$special)) {
+  if (!is.null(dataset_configs$user_correction$special)) {
     data_origin <- switch(
-      configs$user_correction$special,
+      dataset_configs$user_correction$special,
       jcAccount = data_origin %>%
         mutate(
           class = parse_double(str_sub(jcAccount, 6, 7))
@@ -107,20 +101,20 @@ main <- function(loc) {
     select(one_of(key_vars[["user"]])) %>%
     unique()
   # update users table of database
-  copy_to(iquizoo_db, users, "users_to_write")
+  copy_to(iquizoo_db, users, "users_to_write", overwrite = TRUE)
   dbExecute(iquizoo_db, "BEGIN;")
   dbExecute(
     iquizoo_db, "
-REPLACE INTO users
- SELECT *
-   FROM users_to_write;
+    REPLACE INTO users
+    SELECT *
+    FROM users_to_write;
     "
   )
   dbExecute(iquizoo_db, "COMMIT;")
 
   # correction for exercise scores ----
   scores_corrected <- with(
-    configs$score_correction,
+    dataset_configs$score_correction,
     switch(
       type,
       none = data_origin %>%
@@ -197,13 +191,13 @@ REPLACE INTO users
     select(one_of(key_vars[["score"]])) %>%
     filter(!is.na(score))
   # update scores table of database
-  copy_to(iquizoo_db, scores, "scores_to_write")
+  copy_to(iquizoo_db, scores, "scores_to_write", overwrite = TRUE)
   dbExecute(iquizoo_db, "BEGIN;")
   dbExecute(
     iquizoo_db, "
-REPLACE INTO scores
- SELECT *
-   FROM scores_to_write;
+    REPLACE INTO scores
+    SELECT *
+    FROM scores_to_write;
     "
   )
   dbExecute(iquizoo_db, "COMMIT;")
@@ -226,9 +220,9 @@ REPLACE INTO scores
     if (all(scores_with_ability$abId == 0)) break
   }
   ability_scores <- bind_rows(ability_scores_list)
-  if (!is.null(configs$ability_correction)) {
+  if (!is.null(dataset_configs$ability_correction)) {
     ability_scores <- with(
-      configs$ability_correction,
+      dataset_configs$ability_correction,
       switch(
         type,
         modify = {
@@ -254,67 +248,64 @@ REPLACE INTO scores
     )
   }
   # update ability_scores table of database
-  copy_to(iquizoo_db, ability_scores, "ab_scores_to_write")
+  copy_to(iquizoo_db, ability_scores, "ab_scores_to_write", overwrite = TRUE)
   dbExecute(iquizoo_db, "BEGIN;")
   dbExecute(
     iquizoo_db, "
-REPLACE INTO ability_scores
- SELECT *
-   FROM ab_scores_to_write;
+    REPLACE INTO ability_scores
+    SELECT *
+    FROM ab_scores_to_write;
     "
   )
   dbExecute(iquizoo_db, "COMMIT;")
 
   # update additional tables ----
   # update class_covers table of database
-  if (!is.null(configs$cover$class)) {
-    class_covers <- read_excel(file.path(data_dir, configs$cover$class)) %>%
+  if (file.exists(file.path(data_dir, dataset_configs$cover$class))) {
+    class_covers <- read_excel(file.path(data_dir, dataset_configs$cover$class)) %>%
       select(one_of(key_vars[["class_cover"]]))
-    copy_to(iquizoo_db, class_covers, "class_covers_to_write")
+    copy_to(iquizoo_db, class_covers, "class_covers_to_write", overwrite = TRUE)
     dbExecute(iquizoo_db, "BEGIN;")
     dbExecute(
       iquizoo_db, "
-REPLACE INTO class_covers
- SELECT *
-   FROM class_covers_to_write;
+      REPLACE INTO class_covers
+      SELECT *
+      FROM class_covers_to_write;
       "
     )
     dbExecute(iquizoo_db, "COMMIT;")
   }
   # update school_covers table of database
-  if (!is.null(configs$cover$school)) {
-    school_covers <- read_excel(file.path(data_dir, configs$cover$school)) %>%
+  if (file.exists(file.path(data_dir, dataset_configs$cover$school))) {
+    school_covers <- read_excel(file.path(data_dir, dataset_configs$cover$school)) %>%
       select(one_of(key_vars[["school_cover"]]))
-    copy_to(iquizoo_db, school_covers, "school_covers_to_write")
+    copy_to(iquizoo_db, school_covers, "school_covers_to_write", overwrite = TRUE)
     dbExecute(iquizoo_db, "BEGIN;")
     dbExecute(
       iquizoo_db, "
-REPLACE INTO school_covers
- SELECT *
-   FROM school_covers_to_write;
+      REPLACE INTO school_covers
+      SELECT *
+      FROM school_covers_to_write;
       "
     )
     dbExecute(iquizoo_db, "COMMIT;")
-    # update school_details table of database
-    if (!is.null(configs$detail$school)) {
-      school_details <- read_excel(file.path(data_dir, configs$detail$school)) %>%
-        select(one_of(key_vars[["school_detail"]]))
-      copy_to(iquizoo_db, school_details, "school_details_to_write")
-      dbExecute(iquizoo_db, "BEGIN;")
-      dbExecute(
-        iquizoo_db, "
-REPLACE INTO school_details
- SELECT *
-   FROM school_details_to_write;
-      "
-      )
-      dbExecute(iquizoo_db, "COMMIT;")
-    }
+  }
+  # update school_details table of database
+  if (file.exists(file.path(data_dir, dataset_configs$detail$school))) {
+    school_details <- read_excel(file.path(data_dir, dataset_configs$detail$school)) %>%
+      select(one_of(key_vars[["school_detail"]]))
+    copy_to(iquizoo_db, school_details, "school_details_to_write", overwrite = TRUE)
+    dbExecute(iquizoo_db, "BEGIN;")
+    dbExecute(
+      iquizoo_db, "
+        REPLACE INTO school_details
+        SELECT *
+        FROM school_details_to_write;
+        "
+    )
+    dbExecute(iquizoo_db, "COMMIT;")
   }
 }
 
-if (interactive()) {
-  main(readline(prompt = "input the region of your dataset: "))
-} else {
-  main(commandArgs(trailingOnly = TRUE)[1])
-}
+# ensure database is disconnected after processing
+dbDisconnect(iquizoo_db)
