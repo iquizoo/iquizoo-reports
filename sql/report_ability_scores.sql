6df26b78ac0f2df46da0ee201b8c339b71f3380b
Liang Zhang <psychelzh@outlook.com>
2018-07-13 09:36:45 +0800

    Add school details to ability scores view

--- sql/report_ability_scores.sql
+++ sql/report_ability_scores.sql
@@ -1,5 +1,6 @@
 CREATE VIEW IF NOT EXISTS report_ability_scores AS
 SELECT users.userId, users.school, users.grade, users.class,
+       school_details.province, school_details.prefecture, school_details.county,
        school_covers.cover AS school_cover,
        class_covers.cover AS class_cover,
        abilities.name AS ab_name, abilities.name_en AS ab_name_en,
@@ -7,6 +8,9 @@ SELECT users.userId, users.school, users.grade, users.class,
   FROM users
        LEFT JOIN school_covers
        ON users.school = school_covers.school
+
+       LEFT JOIN school_details
+       ON users.school = school_details.school
 
        LEFT JOIN class_covers
        ON users.school = class_covers.school
