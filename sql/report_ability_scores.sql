4f97b950e2500fdae78e25682f0e5b7d119b45e9
Liang Zhang <psychelzh@outlook.com>
2018-08-10 10:00:19 +0800

    Fix an error in select the first participation times for users

--- sql/report_ability_scores.sql
+++ sql/report_ability_scores.sql
@@ -1,18 +1,20 @@
 CREATE VIEW IF NOT EXISTS report_ability_scores AS
 SELECT users.userId, users.school, users.grade, users.class,
-       scores.createTime AS firstPartTime,
+       first_part_time.createTime AS firstPartTime,
        school_details.province || coalesce (school_details.prefecture, "") || school_details.county AS region,
        school_covers.cover AS school_cover,
        class_covers.cover AS class_cover,
        abilities.name AS ab_name, abilities.name_en AS ab_name_en,
        ability_scores.score
   FROM users
-       LEFT JOIN scores
-       ON users.userId = scores.userId
-
-       LEFT JOIN scores AS filter
-       ON filter.userId = scores.userId
-       AND filter.createTime < filter.createTime
+       LEFT JOIN
+            (SELECT *
+               FROM scores
+              WHERE (userId, createTime) IN
+                    (SELECT userId, min(createTime)
+                       FROM scores
+                       GROUP BY userId)) AS first_part_time
+       ON users.userId = first_part_time.userId
 
        LEFT JOIN school_covers
        ON users.school = school_covers.school
@@ -29,6 +31,4 @@ SELECT users.userId, users.school, users.grade, users.class,
        ON users.userId = ability_scores.userId
 
        LEFT JOIN abilities
-       ON ability_scores.abId = abilities.abId
-
- WHERE filter.userId IS NULL;
+       ON ability_scores.abId = abilities.abId;
